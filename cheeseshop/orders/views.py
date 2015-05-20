d552fa88aa8a3feed2733bff395d8ec9a8baa4b3
Liya Kurkov <liyakurkova@gmail.com>
2015-05-19 20:23:30 -0700

    added item.html

--- cheeseshop/orders/views.py
+++ cheeseshop/orders/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 from .models import Item
+from .orderform import ContactForm
 
 class IndexView(generic.ListView):
     template_name = 'orders/index.html'
     context_object_name = 'item_list'
-    
+
     def get_queryset(self):
         """Return the last five cheeses."""
         #Don't even have 5 items yet
@@ -17,3 +19,23 @@ class IndexView(generic.ListView):
 #class DetailView(generic.DetailView):
 #    model = Question
 #    template_name = 'polls/detail.html'
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ContactForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ContactForm()
+
+    return render(request, 'index.html', {'form': form})
+
+
